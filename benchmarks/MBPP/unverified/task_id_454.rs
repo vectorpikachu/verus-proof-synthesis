5b1018b196842ea33d59090e517ff48763bf3cf1
MRHMisu <mrhmisu@gmail.com>
2025-03-17 13:06:46 -0700

    Refactore task_id_454, char to u8

--- benchmarks/MBPP/unverified/task_id_454.rs
+++ benchmarks/MBPP/unverified/task_id_454.rs
@@ -4,13 +4,13 @@ fn main() {}
 
 verus! {
 
-fn contains_z(text: &Vec<char>) -> (result: bool)
+fn contains_z(text: &[u8]) -> (result: bool)
     ensures
-        result == (exists|i: int| 0 <= i < text.len() && (text[i] == 'Z' || text[i] == 'z')),
+        result == (exists|i: int| 0 <= i < text.len() && (text[i] == 90 || text[i] == 122)),
 {
     let mut index = 0;
     while index < text.len() {
-        if text[index] == 'Z' || text[index] == 'z' {
+        if text[index] == 90 || text[index] == 122 {
             return true;
         }
         index += 1;
